86776dc7e7aeaffd2462e6e03764a571c0dac263
Javier <zuniga.javier@seiza-ti.cl>
2023-03-02 22:19:48 -0300

    development initial commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,8 @@ const Home=()=>{
     return(
         <div className="App">
         <header className="App-header">
-
+         <Typography variant='h2' component={'h1'}>Find your new best friend</Typography>
+         <Button>Want to help us?</Button>
         </header>
       </div>
     )
