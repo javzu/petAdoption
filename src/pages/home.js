4d0124db488f4dc796f13e5e4bc5ebf1fcad56ba
Javier <zuniga.javier@seiza-ti.cl>
2023-03-02 22:04:27 -0300

    initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import { Button, Typography } from '@mui/material';
+import '../App.css';
+
+const Home=()=>{
+    
+    return(
+        <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    )
+}
+
+export default Home;
